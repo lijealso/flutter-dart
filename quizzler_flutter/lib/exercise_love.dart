c10e83b176b33feafe950a63166cbc42488e1379
lijealso <lijealso@gmail.com>
2023-04-02 21:03:19 +0100

    love calculator exercise

---
+++ quizzler_flutter/lib/exercise_love.dart
@@ -0,0 +1,20 @@
+﻿import 'dart:math';
+
+// This is a simple Love Calculator
+void main() {
+  print(loveCalculator());
+}
+
+String loveCalculator() {
+  int loveScore = Random().nextInt(100) + 1;
+
+  print(loveScore);
+
+  if (loveScore > 70) {
+    return "You love each other like Kanye loves Kanye";
+  } else if (loveScore >= 50) {
+    return "You like each other";
+  } else {
+    return "You don't like each other";
+  }
+}
