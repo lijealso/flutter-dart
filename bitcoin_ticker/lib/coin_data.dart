7f3c3014794d5b5d645e4693d39fab34b10dbaf5
lijealso <lijealso@gmail.com>
2023-04-22 16:23:40 +0100

    Until 165. Level 2 - Silver Medal

--- bitcoin_ticker/lib/coin_data.dart
+++ bitcoin_ticker/lib/coin_data.dart
@@ -1,3 +1,7 @@
+//2. Import the required packages.
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +32,22 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  //3: Update getCoinData to take the selectedCurrency as an input.
+  Future getCoinData(String selectedCurrency) async {
+    String currency = selectedCurrency.toLowerCase();
+    String requestURL =
+        'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=$currency';
+    http.Response response = await http.get(Uri.parse(requestURL));
+    if (response.statusCode == 200) {
+      var decodedData = jsonDecode(response.body);
+      int lastPrice = decodedData['bitcoin']['$currency'];
+      print(currency);
+      print(lastPrice);
+      return lastPrice.toStringAsFixed(0);
+    } else {
+      print(response.statusCode);
+      throw 'Problem with the get request';
+    }
+  }
+}
